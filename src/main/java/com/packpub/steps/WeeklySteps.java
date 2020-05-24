65f55607c7dc8fe958690cf60455c12867630076
chibaba onyeoka <chinedu.onyeoka@gmail.com>
2020-05-24 21:56:30 +0100

    adding java source to maven project

--- src/main/java/com/packpub/steps/WeeklySteps.java
+++ src/main/java/com/packpub/steps/WeeklySteps.java
@@ -1,6 +1,7 @@
 package com.packpub.steps;
 
 import java.time.DayOfWeek;
+import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -19,4 +20,33 @@ public class WeeklySteps {
         }
         return best;
     }
+    public int getTotalSteps () {
+        int total = 0;
+        for (Steps steps : dailySteps) {
+            total += steps.getSteps();
+        }
+        return total;
+    }
+    public String format() {
+        StringBuilder builder = new StringBuilder();
+        builder.append("Total steps : " + getTotalSteps() + "\n");
+
+        for (Steps steps : dailySteps) {
+            if (dailyGoal.hasMetGoal(steps)) {
+                builder.append("YaY! ");
+            } else {
+                builder.append("  ");
+            }
+            builder.append(steps.getDate().getDayOfWeek());
+            builder.append(" ");
+            builder.append(steps.getSteps());
+            DayOfWeek best =bestDay();
+            if (steps.getDate().getDayOfWeek() == best) {
+                builder.append(" ***** Best DAY!");
+            }
+            builder.append("\n");
+        }
+        return builder.toString();
+    }
+    public void addDailySteps(int Steps, LocalDate date)
 }
