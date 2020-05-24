571d9927872c2d41f2351bd559cb98d9f3f80dcb
chibaba onyeoka <chinedu.onyeoka@gmail.com>
2020-05-24 21:14:38 +0100

    adding java source to maven

--- src/main/java/com/packpub/steps/WeeklySteps.java
+++ src/main/java/com/packpub/steps/WeeklySteps.java
@@ -1,4 +1,22 @@
 package com.packpub.steps;
 
+import java.time.DayOfWeek;
+import java.util.ArrayList;
+import java.util.List;
+
 public class WeeklySteps {
+    List<Steps> dailySteps = new ArrayList<>();
+    DailyGoal dailyGoal;
+
+    public DayOfWeek bestDay() {
+        DayOfWeek best = DayOfWeek.MONDAY;
+        int max = 0;
+        for (Steps steps: dailySteps) {
+            if (steps.getSteps() > max) {
+                max = steps.getSteps();
+                best = steps.getDate().getDayOfWeek();
+            }
+        }
+        return best;
+    }
 }
